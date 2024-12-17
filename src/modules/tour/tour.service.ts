7f935721cae399f67e5f57b98f132035d3ae06c1
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-12-18 03:38:29 +0600

    query builder used

--- src/modules/tour/tour.service.ts
+++ src/modules/tour/tour.service.ts
@@ -1,3 +1,4 @@
+import QueryBuilder from '../../Builder/QueryBuilder';
 import { TTour } from './tour.interface';
 import Tour from './tour.model';
 
@@ -12,34 +13,17 @@ const createTour = async (payload: TTour) => {
 
 const getAllTour = async (query: Record<string, unknown>) => {
 
-    const searchTerm = query?.searchTerm || "";
     const searchAbleFields = ['startLocation', 'locations', 'name', 'slug'];
 
-    const searchQuery = Tour.find({
-        $or: searchAbleFields.map((field) => ({
-            [field]: { $regex: searchTerm, $options: 'i' }
-        }))
-    });
+    const tourQuery = new QueryBuilder(Tour.find(), query)
+        .search(searchAbleFields)
+        .filter()
+        .pagination()
+        .sort()
+        .fields()
 
-    const queryObj = { ...query };
-    const excludeFields = ['searchTerm', 'limit', 'page', 'sort','fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-    const filterQuery = searchQuery.find(queryObj)
+    const result = await tourQuery.modelQuery;
 
-    // pagination
-    console.log(query, queryObj);
-    const page = Number(query?.page) || 1;
-    const limit = Number(query?.limit) || 10;
-    const skip = (page - 1) * limit
-    const paginationQuery = filterQuery.skip(skip).limit(limit);
-
-
-    // sorting
-    const sort = query?.sort || "createdAt"
-    const sortQuery = paginationQuery.sort(sort as string);
-
-    const fields = (query?.fields as string)?.split(',').join(" ") || "-__v";
-    const result = await sortQuery.select(fields);
     return result;
 };
 
